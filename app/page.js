c191168f672aac8649cdd2828bbe07b0b42ee141
Hossam <hossamzahwo31@gmail.com>
2024-12-06 04:09:03 +0200

    Services

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import HeroSection from "./components/heroSection";
+import Services from "./components/services"
+export default function Home() {
+  return (
+    
+    <>
+      <HeroSection />
+      <Services />
+    </>
+  );
+}
